9ce2f5b0e446b51427b4771e007b64f52d299de5
lustojc <sashalusto228@gmail.com>
2021-09-02 20:09:40 +0300

    feat: ready task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.guessedNumber = 0;
+      this.min = 0;
+      this.max = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.guessedNumber = Math.round((this.max + this.min) / 2);
+      return this.guessedNumber;
     }
-
+  
     lower() {
-
+      this.max = this.guessedNumber;
     }
-
+  
     greater() {
-
+      this.min = this.guessedNumber;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
